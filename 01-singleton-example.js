fa5d7fa942d65411016bb7389a8bd849af639058
Fred K. Schott <fredkschott@gmail.com>
2013-12-18 21:00:06 -0800

    Create 01-singleton-example.js

---
+++ 01-singleton-example.js
@@ -0,0 +1,42 @@
+/*
+ * a-singleton.js
+ */
+ 
+// Private
+var x = require('x');
+var y = 'I am private';
+var z = true;
+
+function sum(num1, num2) {
+  return num1 + num2;
+};
+
+
+// Public
+var self = module.exports = {
+
+  someProperty: 'I am public',
+  
+  addFive: function(num) {
+    return sum(num, 5);
+  },
+  
+  toggleZ: function() {
+    z = !z;
+    return z;
+  }
+  
+};
+
+
+
+/*
+ * app.js
+ */
+ var singleton = require('a-singleton');
+ console.log(singleton.x); // undefined (x is private)
+ console.log(singleton.sum(1,2)); // undefined (sum is private)
+ console.log(singleton.someProperty); // 'I am public'
+ console.log(singleton.toggleZ()); // false 
+ console.log(singleton.toggleZ()); // true (public functions can still reference private variables)
+
